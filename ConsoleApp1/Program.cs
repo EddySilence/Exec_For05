d9938b4fe35c251a605b56dbdef6f430941149f1
ispan <ispan@DESKTOP-PAKSETB>
2022-10-19 09:47:40 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			/* 題目：
+			   用迴圈呈現指定右側的結果	
+			   ++++1
+			   +++22
+			   ++333
+			   +4444
+			   55555
+			 */
+
+			for (int i = 1; i <= 5; i++)
+			{
+				char c = Convert.ToChar(48+i);
+				Console.WriteLine(new string('+', 5 - i) + new string(c, i));
+			}
+
+		}
+	}
+}
